eef281b356da724d55fc918357ffa7118ab7fbac
Chris Boyle <cthboyle6@gmail.com>
2021-05-12 16:57:33 -0400

    TODO: need to fix search submission on index page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,6 @@
 class FlatsController < ApplicationController
   def index
-    @flats = Flat.all
+    @flats = Flat.search(params[:search])
   end
 
   def new
@@ -39,6 +39,6 @@ class FlatsController < ApplicationController
   private
 
   def flat_params
-    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :search)
   end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,12 @@
 class Flat < ApplicationRecord
   validates :name, :address, :description, :price_per_night, :number_of_guests, presence: true
   validates :price_per_night, :number_of_guests, numericality: { only_integer: true }
+
+  def self.search(search)
+    if search
+      where("name LIKE '%#{search}%'")
+    else
+      Flat.all
+    end
+  end
 end
