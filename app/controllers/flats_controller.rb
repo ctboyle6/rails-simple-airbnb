622d9d42d8a8204befdc6b937cec05f58907eb16
Chris Boyle <cthboyle6@gmail.com>
2021-05-12 15:32:05 -0400

    created new flat action and form view

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,4 +2,18 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+  end
+
+  private
+
+  def flat_params
+    params.require(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
