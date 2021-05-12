2973903a4594f0b06dd4a0a8d2c85ccb6b6c2127
Chris Boyle <cthboyle6@gmail.com>
2021-05-12 14:42:52 -0400

    added bootstrap, added LW frontend, created flats model controller and view

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,25 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first)''
+
+puts 'clearing old data...'
+
+Flat.destroy_all
+
+puts 'all clean'
+
+puts 'adding some new data'
+
+4.times do
+  flat = Flat.create!(
+    name: Faker::Book.title,
+    address: Faker::Address.full_address,
+    description: Faker::Marketing.buzzwords,
+    price_per_night: rand(200),
+    number_of_guests: rand(6)
+  )
+  puts "Flat #{flat.id} created"
+end
+
+puts 'done!'
